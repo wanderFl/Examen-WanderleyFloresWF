aca44467b271d4d86779253cbf1be2c17c28bd6d
Wanderley Paul Flores Proaño <wanderfhp@hotmail.com>
2024-01-24 08:53:31 -0500

    datos al momentoejecutar WF

--- Examen-WanderleyFloresWF/ViewsWF/paginaWF.xaml.cs
+++ Examen-WanderleyFloresWF/ViewsWF/paginaWF.xaml.cs
@@ -2,49 +2,74 @@ using NewsAPI;
 using NewsAPI.Models;
 using NewsAPI.Constants;
 using System;
+using static System.Net.WebRequestMethods;
+
 namespace Examen_WanderleyFloresWF.ViewsWF;
 
 public partial class paginaWF : ContentPage
 {
-	public paginaWF()
-	{
-		InitializeComponent();
-	}
+    public paginaWF()
+    {
+        InitializeComponent();
+    }
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-        var newsApiClient = new NewsApiClient("14336ebdd12b49a0b8cad0120f273c44");
+        //var newsApiClient = new NewsApiClient("14336ebdd12b49a0b8cad0120f273c44");
+        //var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+
+        string baseUrl = "https://newsapi.org/v2/everything";
+
+        string apiKey = "14336ebdd12b49a0b8cad0120f273c44";
+        string query = "tesla";
+        string sortBy = "" + SortBys.PublishedAt;
+        string language = "" + Languages.EN;
+        DateTime fromDate = new DateTime(2023, 12, 24);
+
+        string apiUrl = $"https://newsapi.org/v2/everything?apiKey=14336ebdd12b49a0b8cad0120f273c44&q=tesla&sortBy=PublishedAt&language=EN&from=2023-12-24";
+
+        var newsApiClient = new NewsApiClient(apiKey);
+
         var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+
         {
+
             Q = "tesla",
             SortBy = SortBys.PublishedAt,
             Language = Languages.EN,
             From = new DateTime(2023, 12, 24)
+
+
         });
         if (articlesResponse.Status == Statuses.Ok)
         {
-            // total results found
+            NoticiasListView.ItemsSource = null;
+
+            var listaDeArticulos = new List<Article>();
+
             Console.WriteLine(articlesResponse.TotalResults);
-            // here's the first 20
+
             foreach (var article in articlesResponse.Articles)
             {
-                Titulo.Text = article.Title;
-                Author.Text= article.Author;
-                Description.Text= article.Description;
-                Url.Text= article.Url;
-                PublishedAt.Text = ""+ article.PublishedAt;
-                // title
-                Console.WriteLine(article.Title);
-                // author
-                Console.WriteLine(article.Author);
-                // description
-                Console.WriteLine(article.Description);
-                // url
-                Console.WriteLine(article.Url);
-                // published at
-                Console.WriteLine(article.PublishedAt);
+                var articulo = new Article
+                {
+                    Title = article.Title,
+                    Author = article.Author,
+                    Description = article.Description,
+                    Url = article.Url,
+                    PublishedAt = article.PublishedAt
+                };
+                listaDeArticulos.Add(articulo);
+
+                Console.WriteLine(articulo.Title);
+                Console.WriteLine(articulo.Author);
+                Console.WriteLine(articulo.Description);
+                Console.WriteLine(articulo.Url);
+                Console.WriteLine(articulo.PublishedAt);
             }
+
+            NoticiasListView.ItemsSource = listaDeArticulos;
         }
-        Console.ReadLine();
     }
+
 }
